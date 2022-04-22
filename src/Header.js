c5ca1a79c266bd0279e65f09a191cbcf2633d149
S. Espinet <s.lou.espinet@gmail.com>
2022-04-22 11:29:46 -0700

    update App component, page title

--- src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header() {
-  return (
-    <h1>Cider Speakeasy</h1>
-  );
-}
-
-export default Header;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import TapList from './TapList';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <TapList />
     </React.Fragment>
   );
 }
