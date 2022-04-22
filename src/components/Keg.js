53036cf944c6cc8eb5856c38d915e0653090ca2d
S. Espinet <s.lou.espinet@gmail.com>
2022-04-22 11:28:42 -0700

    add Keg compoment

---
+++ src/components/Keg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Ticket() {
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+export default Ticket;
