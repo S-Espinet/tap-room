c9f014acee7c84d400a1170f7fe92367b8ec1ff7
S. Espinet <s.lou.espinet@gmail.com>
2022-04-22 11:29:15 -0700

    add TapList component

---
+++ src/components/TapList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Keg from './Keg';
+
+function TapList() {
+  return (
+    <Keg 
+    name="London Dry"
+    brand="Schilling Cider"
+    price="8.00"
+    flavor="Apple - Dry"/>
+  );
+}
+
+export default TapList
